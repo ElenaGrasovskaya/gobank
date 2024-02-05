3e05dfbb2666781e19857449c41c1af359e341dd
elenagrasovskaya <elenagrasovskaya@gmail.com>
2024-02-05 10:16:29 +0200

    Tidied up the API

--- api.go
+++ api.go
@@ -24,6 +24,30 @@ func NewAPIServer(listenAddr string, store Storage) *APIServer {
 	}
 }
 
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	// Defining routes
+	router.HandleFunc("/login", makeHTTPHandleFunc(s.handleLogin)).Methods(http.MethodPost)
+	router.HandleFunc("/expense", withJWTAuth(makeHTTPHandleFunc(s.handleCreateExpense), s.store)).Methods(http.MethodPost)
+	router.HandleFunc("/expense", withJWTAuth(makeHTTPHandleFunc(s.handleGetExpenseForUser), s.store)).Methods(http.MethodGet)
+	router.HandleFunc("/expenses", makeHTTPHandleFunc(s.handleGetAllExpense)).Methods(http.MethodGet)
+	router.HandleFunc("/register", makeHTTPHandleFunc(s.handleRegister)).Methods(http.MethodPost)
+	router.HandleFunc("/logout", makeHTTPHandleFunc(s.handleLogout)).Methods(http.MethodPost)
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleGetAccount)).Methods(http.MethodGet)
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleCreateAccount)).Methods(http.MethodPost)
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleDeleteAccount)).Methods(http.MethodDelete)
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleGetAccountById), s.store)).Methods(http.MethodGet)
+
+	// Apply the CORS middleware to the router
+	corsRouter := corsMiddleware(router)
+
+	fmt.Println("JSON API server is running on port: " + s.listenAddr)
+	http.ListenAndServe(s.listenAddr, corsRouter)
+}
+
+// ACCOUNT MANAGING *********************************************************
+
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
 
 	accounts, err := s.store.GetAccounts()
@@ -33,31 +57,6 @@ func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) err
 	}
 
 	return WriteJSON(w, http.StatusOK, accounts)
-}
-
-func (s *APIServer) handleGetAllExpense(w http.ResponseWriter, r *http.Request) error {
-
-	expenses, err := s.store.GetAllExpense()
-
-	if err != nil {
-		return err
-	}
-
-	return WriteJSON(w, http.StatusOK, expenses)
-}
-
-func (s *APIServer) handleGetExpense(w http.ResponseWriter, r *http.Request) error {
-	userId, err := getIdFrommCookie(w, r)
-	if err != nil {
-		return err
-	}
-	expenses, err := s.store.GetExpenseForUser(userId)
-
-	if err != nil {
-		return err
-	}
-
-	return WriteJSON(w, http.StatusOK, expenses)
 }
 
 func (s *APIServer) handleGetAccountById(w http.ResponseWriter, r *http.Request) error {
@@ -104,13 +103,52 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	return WriteJSON(w, http.StatusOK, account)
 }
 
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	id, err := getId(r)
+	if err != nil {
+		return err
+	}
+
+	if err := s.store.DeleteAccount(id); err != nil {
+		return err
+	}
+	return WriteJSON(w, http.StatusOK, map[string]int{"deleted": id})
+}
+
+//EXPENCE SERVICES ****************************************************************************
+
+func (s *APIServer) handleGetAllExpense(w http.ResponseWriter, r *http.Request) error {
+
+	expenses, err := s.store.GetAllExpense()
+
+	if err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, expenses)
+}
+
+func (s *APIServer) handleGetExpenseForUser(w http.ResponseWriter, r *http.Request) error {
+	userId, err := getIdFromCookie(w, r)
+	if err != nil {
+		return err
+	}
+	expenses, err := s.store.GetExpenseForUser(userId)
+
+	if err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, expenses)
+}
+
 func (s *APIServer) handleCreateExpense(w http.ResponseWriter, r *http.Request) error {
 	createExpenseRequest := new(CreateExpenseRequest)
 	if err := json.NewDecoder(r.Body).Decode(createExpenseRequest); err != nil {
 		return err
 	}
 
-	userId, err := getIdFrommCookie(w, r)
+	userId, err := getIdFromCookie(w, r)
 	if err != nil {
 		return err
 	}
@@ -127,27 +165,7 @@ func (s *APIServer) handleCreateExpense(w http.ResponseWriter, r *http.Request) 
 	return WriteJSON(w, http.StatusOK, expense)
 }
 
-func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
-	id, err := getId(r)
-	if err != nil {
-		return err
-	}
-
-	if err := s.store.DeleteAccount(id); err != nil {
-		return err
-	}
-	return WriteJSON(w, http.StatusOK, map[string]int{"deleted": id})
-}
-
-func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-	transferReq := new(TransferRequest)
-
-	if err := json.NewDecoder(r.Body).Decode(transferReq); err != nil {
-		return err
-	}
-	defer r.Body.Close()
-	return WriteJSON(w, http.StatusOK, transferReq)
-}
+//AUTHORIZATION SERVICES**********************************************************
 
 func (s *APIServer) handleLogin(w http.ResponseWriter, r *http.Request) error {
 
@@ -211,70 +229,6 @@ func (s *APIServer) handleLogout(w http.ResponseWriter, r *http.Request) error {
 	return WriteJSON(w, http.StatusLocked, "User Logged Out")
 }
 
-func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	fmt.Printf("This is the method %s", r.Method)
-	if r.Method == "GET" {
-		return s.handleGetAccount(w, r)
-	}
-
-	if r.Method == "POST" {
-		return s.handleCreateAccount(w, r)
-	}
-
-	if r.Method == "DELETE" {
-		return s.handleDeleteAccount(w, r)
-	}
-	return fmt.Errorf("method not allowed %s", r.Method)
-}
-
-func (s *APIServer) Run() {
-	router := mux.NewRouter()
-
-	// Defining routes
-	router.HandleFunc("/login", makeHTTPHandleFunc(s.handleLogin)).Methods(http.MethodPost)
-	router.HandleFunc("/expense", withJWTAuth(makeHTTPHandleFunc(s.handleCreateExpense), s.store)).Methods(http.MethodPost)
-	router.HandleFunc("/expense", withJWTAuth(makeHTTPHandleFunc(s.handleGetExpense), s.store)).Methods(http.MethodGet)
-	router.HandleFunc("/expenses", makeHTTPHandleFunc(s.handleGetAllExpense)).Methods(http.MethodGet)
-	router.HandleFunc("/register", makeHTTPHandleFunc(s.handleRegister)).Methods(http.MethodPost)
-	router.HandleFunc("/logout", makeHTTPHandleFunc(s.handleLogout)).Methods(http.MethodPost)
-	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleGetAccountById), s.store)).Methods(http.MethodGet)
-	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
-
-	// Apply the CORS middleware to the router
-	corsRouter := corsMiddleware(router)
-
-	fmt.Println("JSON API server is running on port: " + s.listenAddr)
-	http.ListenAndServe(s.listenAddr, corsRouter)
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-		// Check if it's a preflight request
-		if r.Method == "OPTIONS" {
-			// Send response for preflight request
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		// Serve the next handler
-		next.ServeHTTP(w, r)
-	})
-}
-
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(status)
-	return json.NewEncoder(w).Encode(v)
-}
-
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
 func createJWT(account *Account) (string, error) {
 	claims := &jwt.MapClaims{
 		"expiresAt":     15000,
@@ -284,10 +238,6 @@ func createJWT(account *Account) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString([]byte(secrtet))
-}
-
-func PermissionDenied(w http.ResponseWriter) {
-	WriteJSON(w, http.StatusForbidden, ApiError{Error: "Invalid Token "})
 }
 
 func withJWTAuth(handlerFunc http.HandlerFunc, s Storage) http.HandlerFunc {
@@ -334,64 +284,14 @@ func withJWTAuth(handlerFunc http.HandlerFunc, s Storage) http.HandlerFunc {
 	}
 }
 
-func getIdFrommCookie(w http.ResponseWriter, r *http.Request) (int, error) {
-	cookies, err := r.Cookie("token")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	token, err := validateJWT(cookies.Value)
-	if err != nil {
-		PermissionDenied(w)
-		return 0, err
-	}
-
-	if !token.Valid {
-		PermissionDenied(w)
-		return 0, err
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		id := claims["id"].(float64)
-		return int(id), nil
-
-	} else {
-		PermissionDenied(w)
-		return 0, err
-	}
-}
-
 func validateJWT(tokenString string) (*jwt.Token, error) {
 	secret := os.Getenv("JWT_SECRET")
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(secret), nil
 	})
-}
-
-type ApiError struct {
-	Error string `json: "error"`
-}
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
-		}
-	}
-}
-
-func getId(r *http.Request) (int, error) {
-	idStr := mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		return id, fmt.Errorf("invalid id given %s", idStr)
-	}
-
-	return id, nil
 }
 
 func setSession(userId int, userEmail string, response http.ResponseWriter) {
@@ -436,4 +336,87 @@ func EncrPassword(reqPassword string, dbPassword string) (bool, error) {
 		return true, nil
 	}
 
+}
+
+//UTILS  ********************************************************************************************
+
+func getId(r *http.Request) (int, error) {
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return id, fmt.Errorf("invalid id given %s", idStr)
+	}
+
+	return id, nil
+}
+
+func getIdFromCookie(w http.ResponseWriter, r *http.Request) (int, error) {
+	cookies, err := r.Cookie("token")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	token, err := validateJWT(cookies.Value)
+	if err != nil {
+		PermissionDenied(w)
+		return 0, err
+	}
+
+	if !token.Valid {
+		PermissionDenied(w)
+		return 0, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		id := claims["id"].(float64)
+		return int(id), nil
+
+	} else {
+		PermissionDenied(w)
+		return 0, err
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func PermissionDenied(w http.ResponseWriter) {
+	WriteJSON(w, http.StatusForbidden, ApiError{Error: "Invalid Token "})
+}
+
+type ApiError struct {
+	Error string `json: "error"`
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		// Check if it's a preflight request
+		if r.Method == "OPTIONS" {
+			// Send response for preflight request
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Serve the next handler
+		next.ServeHTTP(w, r)
+	})
 }
