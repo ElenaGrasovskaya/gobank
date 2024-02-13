2ac78273f881eb74bdef4e38bf14bef347eaf4fe
elenagrasovskaya <elenagrasovskaya@gmail.com>
2024-02-13 11:10:07 +0200

    tests started

--- main.go
+++ main.go
@@ -7,20 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-/* func main() {
-	store, err := NewPostgresStore()
-	if err != nil {
-		log.Fatal(err)
-		fmt.Println("store")
-	}
-
-	if err := store.Init(); err != nil {
-		log.Fatal(err)
-	}
-	server := NewAPIServer(":3000", store)
-	server.Run()
-} */
-
 func main() {
 	store, err := NewPostgresStore()
 	if err != nil {
